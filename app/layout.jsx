56604314b53e52fce3fb6f2c68b798b0f07c6f73
NizarNaser <nizarnaser17@gmail.com>
2025-08-31 03:27:06 +0200

    add icons commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,14 +8,14 @@ export const metadata = { title: 'Tax Invoices App' };
 
 
 export default function RootLayout({ children }) {
-return (
-<html lang="ar" dir="rtl">
-<body>
-<Providers>{/* Navbar can show login */}
-<Navbar />
-<main className="container mx-auto p-4">{children}</main>
-</Providers>
-</body>
-</html>
-);
+    return (
+        <html lang="ar" dir="rtl">
+            <body>
+                <Providers>{/* Navbar can show login */}
+                    <Navbar />
+                    <main className="container mx-auto p-4">{children}</main>
+                </Providers>
+            </body>
+        </html>
+    );
 }
